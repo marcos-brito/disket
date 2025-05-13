458793b937430f96aa1c892abf86d802d54b1d52
Marcos Brito <marcos.brito0507@gmail.com>
2025-05-13 15:41:10 -0300

    doc: write better mod level docs for os

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -1,2 +1,8 @@
+//! Platform-specific extensions
+//!
+//! Provides access to platform-specific information and functionality, supporting tasks that a
+//! platform-agnostic abstraction layer can not. You shouldn't expect any extensive documentation
+//! for each platform here. Check specific `man pages` and `APIs` for that.
+
 #[cfg(feature = "mount")]
 pub mod mount;
